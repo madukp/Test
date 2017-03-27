b34d3a1d20c7d6cb8e674940c149816d75070f47
madurajith.p <madurajith.p@MADURAJITH-LAP.sl.aeturnum.com>
2017-03-27 21:26:14 +0530

    Structural change of features

--- src/test/java/com/aet/framework/cucumberTest/TestRunner.java
+++ src/test/java/com/aet/framework/cucumberTest/TestRunner.java
@@ -1,15 +1,24 @@
 package com.aet.framework.cucumberTest;
 
+
 import org.junit.runner.RunWith;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
- 
+
 @RunWith(Cucumber.class)
+
 @CucumberOptions(
 		plugin = { "pretty", "html:target/cucumber-html-report", "json:target/cucumber-report.json" }, 
-		features = { "src/test/resources/feature/Signin.feature","src/test/resources/feature/Home.feature" }, 
-		tags = { "@CreateItem " }
-		)
+		features = { "src/main/resources/cucumberFeatures/CreateItem.feature" }, 
+		glue={
+						"com.aet.framework.cucumberStepDefinitions"
+		},
+		//tags = { "@test","@BVT" }
+				tags = { "@CreateItem " }
+		
+)
+
 
 public class TestRunner {
 
