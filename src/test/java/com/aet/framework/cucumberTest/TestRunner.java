1da9b21228ece78d9315af5e0a709786a2f7d098
madurajith.p <madurajith.p@MADURAJITH-LAP.sl.aeturnum.com>
2017-04-15 14:04:00 +0530

    cucumberFeatures - test runner updated

--- src/test/java/com/aet/framework/cucumberTest/TestRunner.java
+++ src/test/java/com/aet/framework/cucumberTest/TestRunner.java
@@ -10,7 +10,7 @@ import cucumber.api.junit.Cucumber;
 
 @CucumberOptions(
 		plugin = { "pretty", "html:target/cucumber-html-report", "json:target/cucumber-report.json" }, 
-		features = {"src/main/resources/cucumberFeatures/temp" }, 
+		features = {"src/main/resources/cucumberFeatures" }, 
 		glue={
 						"com.aet.framework.cucumberStepDefinitions"
 		}
